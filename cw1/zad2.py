339cd5d673ce81533f75f667ba2d40e09279c7de
Lasek25 <Lasek25@users.noreply.github.com>
2019-10-17 23:15:01 +0200

    nowa wersja programu

---
+++ cw1/zad2.py
@@ -0,0 +1,7 @@
+imie = "Krzysztof"
+nazwisko = "Laskowski"
+litera_1=imie[1]
+litera_2=nazwisko[2]
+l1=imie.count(litera_1)
+l2=nazwisko.count(litera_2)
+print("Ilość liter " + litera_1 + " w imieniu wynosi: " + str(l1) + ", a ilość liter " + litera_2 + " w nazwisku: " + str(l2))
