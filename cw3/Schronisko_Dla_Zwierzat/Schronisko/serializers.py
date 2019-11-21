0d4db459cb43772a50cf2303ab52f9d573e5ca0c
KrzysztofLaskowski <local@WMII-DKCM48L7JS>
2019-11-21 14:41:32 +0100

    serializacja zwierze

--- cw3/Schronisko_Dla_Zwierzat/Schronisko/serializers.py
+++ cw3/Schronisko_Dla_Zwierzat/Schronisko/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
+from .models import *
 
-# class ZwierzeSerializer(serializers.Serializer){
-#     fields = ('gatunek', 'imie', 'rasa', 'plec', 'data_przyjecia')
-# }
+
+class ZwierzeSerializer(serializers.Serializer):
+    id = serializers
+    gatunek = serializers.CharField(max_length=4, choices=Zwierze.GATUNEK_CHOICES)
+    imie = serializers.CharField(max_length=45)
