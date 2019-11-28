78e0a661b3fa4e8f5c8d18491c3e503128081520
KrzysztofLaskowski <local@WMII-DKCM48L7JS>
2019-11-28 14:37:40 +0100

    serializacja

--- cw3/Schronisko_Dla_Zwierzat/Schronisko/serializers.py
+++ cw3/Schronisko_Dla_Zwierzat/Schronisko/serializers.py
@@ -2,7 +2,36 @@ from rest_framework import serializers
 from .models import *
 
 
-class ZwierzeSerializer(serializers.Serializer):
-    id = serializers
-    gatunek = serializers.CharField(max_length=4, choices=Zwierze.GATUNEK_CHOICES)
-    imie = serializers.CharField(max_length=45)
+class ZwierzeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Zwierze
+        fields = ['id', 'gatunek', 'imie', 'rasa', 'plec', 'data_przyjecia',
+                  'pracownik', 'boks']
+    # id = serializers
+    # gatunek = serializers.CharField(max_length=4, choices=Zwierze.GATUNEK_CHOICES)
+    # imie = serializers.CharField(max_length=45)
+
+
+class BoksSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Boks
+        fields = ['id', 'strefa', 'pracownik']
+        # fields = '__all__'
+
+
+class PracownikSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pracownik
+        fields = ['id', 'imie', 'nazwisko', 'pesel', 'telefon', 'umowa']
+
+
+class UmowaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Umowa
+        fields = ['id', 'data_zatrudnienia', 'data_konca_umowy', 'nr_konta', 'typ_umowy']
+
+
+class TypUmowySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Typ_Umowy
+        fields = ['id', 'stanowisko', 'pensja']
