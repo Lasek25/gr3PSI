69ee5cf87d4022fae472682fb782dae009fa5ad8
Krzysztof Laskowski <kp.laskowski250@gmail.com>
2019-12-07 22:58:29 +0100

    powrot do stabilnej wersji

--- cw3/Schronisko_Dla_Zwierzat/Schronisko/urls.py
+++ cw3/Schronisko_Dla_Zwierzat/Schronisko/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('index', views.index, name='index'),
+<<<<<<< HEAD:Schronisko_Dla_Zwierzat/Schronisko/urls.py
+    path('pracownicy/', views.pracownik, name='pracownik'),
+=======
     path('pracownicy/', views.PracownikLista, name='pracownik'),
+>>>>>>> origin/master:cw3/Schronisko_Dla_Zwierzat/Schronisko/urls.py
 ]
