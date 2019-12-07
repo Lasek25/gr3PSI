0d1f0cb799072a0a66fdefa083319d45d390a799
Krzysztof Laskowski <kp.laskowski250@gmail.com>
2019-12-07 20:47:06 +0100

    Revert "serializacja"
    This reverts commit 78e0a661b3fa4e8f5c8d18491c3e503128081520.

--- cw3/Schronisko_Dla_Zwierzat/Schronisko_Dla_Zwierzat/urls.py
+++ cw3/Schronisko_Dla_Zwierzat/Schronisko_Dla_Zwierzat/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('schronisko/', include('Schronisko.urls')),
-    # path(r'^api-auth/', include('rest_framework.urls'))
 ]
