3b135af12a8e0480f5d790abe9d1169f892fc07e
Krzysztof Laskowski <kp.laskowski250@gmail.com>
2020-01-16 14:34:51 +0100

    widok pracownik

--- cw3/Schronisko_Dla_Zwierzat/Schronisko/views.py
+++ cw3/Schronisko_Dla_Zwierzat/Schronisko/views.py
@@ -33,6 +33,34 @@ class PracownikLista(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class PracownikSzczegoly(APIView):
+    permission_classes = [permissions.IsAdminUser]
+
+    def get_object(self, pk):
+        try:
+            return Pracownik.objects.get(pk=pk)
+        except Pracownik.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        question = self.get_object(pk)
+        serializer = PracownikSerializer(question)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        question = self.get_object(pk)
+        serializer = PracownikSerializer(question, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        question = self.get_object(pk)
+        question.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = PracownikSerializer
